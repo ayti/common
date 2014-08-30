6df238e08aa8bc7426567235ceb5097439111922
ayti <ayti@ninety-nine.ch>
2014-08-30 17:39:56 +0200

    b1 test 1

---
+++ HelloWorld/src/ch/alune/test/helloworld/DateGetter.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ch.alune.test.helloworld;
+
+import java.util.Date;
+
+/**
+ * @author Pascal
+ *
+ */
+public class DateGetter {
+	
+	@Override
+	public String toString() {
+		return new Date().toString();
+	}
+
+}
