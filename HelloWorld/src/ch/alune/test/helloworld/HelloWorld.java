908e57b1b15c4cfea14d4f5a5f2d0683926b1755
ayti <ayti@ninety-nine.ch>
2014-08-30 17:33:06 +0200

    first init

---
+++ HelloWorld/src/ch/alune/test/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ch.alune.test.helloworld;
+
+/**
+ * @author Pascal
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Wolrd :-)");
+	}
+
+}
